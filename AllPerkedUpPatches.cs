8659bfd48c7acf076e425ae76ddfe0e13b577d4c
binbinmods <binbinmods@gmail.com>
2025-11-02 23:31:28 -0500

    Initial Commit

---
+++ AllPerkedUpPatches.cs
@@ -0,0 +1,51 @@
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+// using static Obeliskial_Essentials.Essentials;
+using System;
+using static ModNameToReplace.Plugin;
+using static ModNameToReplace.CustomFunctions;
+using static ModNameToReplace.ModNameToReplaceFunctions;
+using System.Collections.Generic;
+using static Functions;
+using UnityEngine;
+// using Photon.Pun;
+using TMPro;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Diagnostics;
+// using Unity.TextMeshPro;
+
+// Make sure your namespace is the same everywhere
+namespace ModNameToReplace
+{
+
+    [HarmonyPatch] // DO NOT REMOVE/CHANGE - This tells your plugin that this is part of the mod
+
+    public class ModNameToReplacePatches
+    {
+        public static bool devMode = false; //DevMode.Value;
+        public static bool bSelectingPerk = false;
+        public static bool IsHost()
+        {
+            return GameManager.Instance.IsMultiplayer() && NetworkManager.Instance.IsMaster();
+        }
+
+
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(PerkTree), "CanModify")]
+        public static void CanModifyPostfix(ref bool __result)
+        {
+            if (IsHost() ? EnablePerkChangeWhenever.Value : EnablePerkChangeWheneverMP)
+                __result = true;
+        }
+
+        
+
+    }
+}
